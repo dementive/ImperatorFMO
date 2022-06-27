252001f746c541e43cda4084fedff21e745f48ed
dementive <nathanvandrei@yahoo.com>
2022-06-27 18:27:15 -0400

    Religious conversion cheaper

--- python/bloodlines.py
+++ python/bloodlines.py
@@ -1,4 +1,4 @@
-def print_bloodlines_scripted_lists(bloodlines):
+﻿def print_bloodlines_scripted_lists(bloodlines):
 	openbracket = "{" #hmm
 	closebracket = "}"
 	scripted_lists = "{bloodlines} = {openbracket}\n	base = living_character\n	conditions = {openbracket}\n		has_trait = {bloodlines}\n	{closebracket}\n{closebracket}".format(openbracket=openbracket,closebracket=closebracket,bloodlines=bloodlines)
@@ -11,6 +11,8 @@ def print_bloodlines_scripted_guis(bloodlines):
 	print(scripted_guis)
 
 bloodlines = ["alcimachid","antigonids","antipatrid","argeads","lagids","seleucids","aeacidae","agiad","achaemenid","maurya","zadok","agathocles","orontid","atropates","barca","arsaces","spartocid","sophytid","diodotus","brennus","illyrius","claudii","cornelii","fabii","demosthenid","mithridates","superbus","vercingetorix","chola","pandya","chera","kalinga","magonid","hannonid","dido","blood_of_porus","blood_of_vijaya","blood_of_zipoetes","ariovistid","battiad","blood_of_menander","blood_of_bharata","blood_of_aratus","blood_of_david", "magas"]
+
+chinese_bloodlines = ["qin_bloodline","chu_bloodline","wei_bloodline","zhou_bloodline","yue_bloodline","yan_bloodline","song_bloodline","lu_bloodline","wii_bloodline","han_bloodline","zhao_bloodline","zhongshan_bloodline","qii_bloodline","nii_bloodline","zou_bloodline","teng_bloodline","zhaoxian_bloodline"]
 
 for i in bloodlines:
 	#print_bloodlines_scripted_lists(i)
---
+++ python/hybrid_cultures.py
@@ -0,0 +1,19 @@
+﻿def PrintList(list):
+	for current_culture in list:
+		OutputList(list)
+
+def OutputList(list):
+	done_cultures = []
+	for culture in list:
+		upper_culture = culture.title()
+		print(f"\n#---------------------------------------\n# Culture: {upper_culture}\n#---------------------------------------\n")
+		for hybrid_culture in list:
+			if ( hybrid_culture != culture and hybrid_culture not in culture):
+				upper_hybrid = hybrid_culture.title()
+				print(f"{culture}-{hybrid_culture} = {{}}")
+		done_cultures.append(culture)
+
+hellenic_cultures =  ["macedonian","italiotian","thracian","cretan","achaean","argolian","arcadian","epirote","boeotian","peloponnesian","athenian","euboean","aetolian","aegean","aeolian","troan","ionian","cypriot","thessalian","bosporan","syracusan","cyrenaican","massalian"]
+
+
+PrintList(hellenic_cultures)
